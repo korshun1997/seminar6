5d5d7a36825adb3d78d8afabfd1e03dd1539df33
korshun1997 <116491146+korshun1997@users.noreply.github.com>
2022-12-03 22:48:55 +0300

    Add files via upload
    семинар6

---
+++ 41/Program.cs
@@ -0,0 +1,22 @@
+﻿// 
+Console.WriteLine("Введите М");
+int M = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int count = 0;
+int[] array = new int[M];
+Random rand = new Random();
+int pos(int i, int M, int count)
+{
+    for (i=0; i<M; i++)
+{
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+return count;
+}
+count = pos(i, M, count);
+Console.WriteLine();
+Console.WriteLine($"{count}");
---
+++ 43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите k1, b1, k2, b2");
+int k1 = Convert.ToInt32(Console.ReadLine()); 
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+int y1 = k1*x+b1;
+int y2 = k2*x+b2;
+double x0 = 0;
+double y0 = 0;
+double Point(int k1, int b1, int k2, int b2, double x0, double y0)
+{
+    x0 = (b2-b1)/(k1-k2);
+    return x0;
+}
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    Point(k1, b1, k2, b2, x0, y0);
+    y0 = k2*x0+b2; 
+    Console.WriteLine($"{x0}; {y0}");
+}
